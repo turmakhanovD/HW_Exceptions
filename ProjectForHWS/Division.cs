f0d60b7cdc79df3f68f4c482525a5ac0d532a000
Islam <denizzz9998882@gmail.com>
2019-02-01 18:55:55 +0600

    Adding first files

---
+++ ProjectForHWS/Division.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForHWS
+{
+    public class Division
+    {
+        public int MakeDivision(int arg1,int arg2)
+        {
+            try
+            {
+                if (arg1 == null)
+                {
+                    throw new ArgumentNullException("Вы не передали значение!");
+                }
+
+                if (arg2 <= 0)
+                {
+                    throw new ArgumentException("Мы не можем делить на ноль или на отрицательное число!");
+                }
+
+                return (arg1 / arg2);
+            }
+
+            catch
+            {
+                 Console.WriteLine("Error");
+                 return 0;
+            }
+        }
+    }
+}
---
+++ ProjectForHWS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForHWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division division = new Division();
+            Console.WriteLine( division.MakeDivision(25, 5) ); 
+            Console.WriteLine( division.MakeDivision(25, 0) );
+
+            int[] mas = { 5, 6, 765, 2342, 111, 444, 11234 };
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write(mas[i] + "\t");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выход за границы!");
+            }
+            finally
+            {
+                Console.WriteLine("Обработка завершена!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
